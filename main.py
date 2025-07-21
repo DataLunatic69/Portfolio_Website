e4c8740ca84b4da7f5b60e77ed87b2cff5775d62
DataLunatic69 <amansingh2002.ap@gmail.com>
2025-07-21 12:45:39 +0530

    contact api tested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py
 from fastapi import FastAPI
-from server.contact.routes import router as contact_router
+from server.contact.routes import  contact_router
 
 app = FastAPI()
 app.include_router(contact_router)
